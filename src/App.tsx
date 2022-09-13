c056d086a05a9a8fabd4fc819f1b781552ecf6e2
alexf <alex.feller26@gmail.com>
2022-09-13 18:33:47 -0400

    created work section and adjust section styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.module.scss';
 import Header from './components/Header';
 import About from './components/About';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className={styles.container}>
         <About />
+        <Work />
       </div>
     </div>
   );
---
+++ src/components/Work.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Work.module.scss';
+// import Button from './Button';
+
+function Work() {
+  return (
+    <div className={styles.workcontainer}>
+      <div className={styles.worksection}>
+        <h2>View my Work</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
