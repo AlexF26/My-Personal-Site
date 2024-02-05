a7436191fa06332e5fb6e36986b75c42e23da6bd
Alex F <alex.feller26@gmail.com>
2024-02-05 18:27:05 -0500

    feat: init main GA4 tracking

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { About } from './components/about/About';
 import { Work } from './components/work/Work';
 import { Resume } from './components/resume/Resume';
 import Footer from './components/footer/Footer';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-FKGSNK7Z6Z');
 
 const App = () => {
   return (
